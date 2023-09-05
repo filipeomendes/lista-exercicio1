2e828a5f5b4818f84a7d0a916b1795f18a3478d5
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-09-05 20:15:41 -0300

    Ajustando erros restantes no react

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
@@ -9,22 +9,24 @@ import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 
+
+
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/" ,
     element: <App/>,
     errorElement: <Error/>,
     children:[
       {
-        path:"/",
+        path: "/" ,
         element: <Home/>
       },
       {
-        path: "/produtos",
+        path: "/produtos" ,
         element: <Produtos/>
       },
       {
-        path: "/produtos/editar/:id",
+        path: "/produtos/editar/:id" ,
         element: <EditarProdutos/>
       },
       {
@@ -37,6 +39,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
