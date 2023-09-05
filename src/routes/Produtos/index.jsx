5707a2310c39febab2c897a096b53b63880ad9b5
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-09-05 17:17:41 -0300

    Passando os valores pela URI

--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,17 @@
+import { listaProdutos } from "../listaProdutos";
+
 export default function Produtos(){
 
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
-    )
+)
 }
